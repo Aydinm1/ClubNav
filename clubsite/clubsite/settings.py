67093c3b6a423402006b04dda4d09c24bf35e797
Aydin Merchant <unconfigured@null.spigotmc.org>
2023-04-02 19:40:20 -0500

    [Legacy commit squashed]

--- clubsite/clubsite/settings.py
+++ clubsite/clubsite/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'clubsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 

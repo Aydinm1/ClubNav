6b00c99ab6a74b2762a981399cc6e56f402332e5
a-b-e-q <abequinones@icloud.com>
2023-04-07 16:07:48 -0500

    [Legacy commit squashed]

--- clubsite/list/admin.py
+++ clubsite/list/admin.py
@@ -3,9 +3,12 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Club
-from .forms import ClubAdminForm
+from django.db import models
+from django.forms import CheckboxSelectMultiple
 
 class ClubAdmin(admin.ModelAdmin):
-    form = ClubAdminForm
+    formfield_overrides = {
+        models.ManyToManyField: {'widget': CheckboxSelectMultiple}
+    }
 
 admin.site.register(Club, ClubAdmin)

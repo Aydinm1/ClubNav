091c63532d8f42697a090882071e5372b8f86e43
a-b-e-q <abequinones@icloud.com>
2023-04-07 14:22:18 -0500

    [Legacy commit squashed]

--- clubsite/list/models.py
+++ clubsite/list/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib import admin
 
-# Create your models here.
+# Create your models here...
 
 class Club(models.Model):
     name = models.CharField(max_length=50)

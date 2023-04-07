c46ddf37a903673c21d9520ce72962874eb59d9e
Aydin Merchant <unconfigured@null.spigotmc.org>
2023-04-07 11:44:40 -0500

    [Legacy commit squashed]

--- clubsite/list/admin.py
+++ clubsite/list/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Club
+from .models import Club, ClubAdminForm
+
+class ClubAdmin(admin.ModelAdmin):
+    form = ClubAdminForm
 
 admin.site.register(Club)

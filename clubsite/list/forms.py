d608abb563eedf416cc0c09869c73738941e43fc
a-b-e-q <abequinones@icloud.com>
2023-04-07 13:20:41 -0500

    [Legacy commit squashed]

--- clubsite/list/forms.py
+++ clubsite/list/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Club
 
 class ClubAdminForm(forms.ModelForm):
-    days = forms.MultipleChoiceField(choices = Club.meeting_days_choices, widget=forms.CheckboxSelectMultiple())
+    meeting_days = forms.MultipleChoiceField(choices = Club.meeting_days_choices, widget=forms.CheckboxSelectMultiple())
 
     class Meta:
         model = Club

5462e9b8d34f519a4010e97bccfa228d29d6b80b
a-b-e-q <abequinones@icloud.com>
2023-04-07 20:48:09 -0500

    [Legacy commit squashed]

--- clubsite/list/views.py
+++ clubsite/list/views.py
@@ -1,4 +1,4 @@
-import random
+import random, itertools
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,13 +6,14 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from .models import Club
-# from django.template import loader
+from django.template import loader
 
 latest_id = Club.objects.latest('id')
 
 def index(request):
-    random_club = Club.objects.get(id=random.randrange(1, latest_id))
-    # template = loader.get_template("list/index.html")
+    for _ in range(4):
+        random_club = Club.objects.get(id=random.randrange(1, latest_id))
+    template = loader.get_template("list/index.html")
     return HttpResponse("Hello, world. You're at the polls index.")
 
 print(latest_id)

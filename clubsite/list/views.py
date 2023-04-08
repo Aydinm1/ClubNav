7d789dfa0b241c99fc7a90983bb7d3222a3ba899
a-b-e-q <abequinones@icloud.com>
2023-04-07 20:35:25 -0500

    [Legacy commit squashed]

--- clubsite/list/views.py
+++ clubsite/list/views.py
@@ -1,13 +1,18 @@
+import random
 from django.shortcuts import render
 
 # Create your views here.
 # Request handler - request -> response
 
 from django.http import HttpResponse
+from .models import Club
 # from django.template import loader
-# Deciding whether or not to use the template system 
 
+latest_id = Club.objects.latest('id')
 
 def index(request):
+    random_club = Club.objects.get(id=random.randrange(1, latest_id))
     # template = loader.get_template("list/index.html")
     return HttpResponse("Hello, world. You're at the polls index.")
+
+print(latest_id)

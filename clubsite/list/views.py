463d59b63ddd596a570918b8227a6147419250f3
a-b-e-q <abequinones@icloud.com>
2023-04-18 21:42:59 -0500

    [Legacy commit squashed]

--- clubsite/list/views.py
+++ clubsite/list/views.py
@@ -8,8 +8,6 @@ from django.http import HttpResponse
 from .models import Club
 from django.template import loader
 
-latest_id = Club.objects.latest('id')
-random_club = Club.objects.get(id=random.sample(latest_id, 3))
 
 def index(request):   
     template = loader.get_template("list/index.html")

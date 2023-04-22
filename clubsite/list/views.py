a2a6fc47fb3a0a7e592edf259451c3214af0785b
a-b-e-q <27394198+a-b-e-q@users.noreply.github.com>
2023-04-22 15:54:03 -0500

    [Legacy commit squashed]

--- clubsite/list/views.py
+++ clubsite/list/views.py
@@ -1,14 +1,8 @@
-import random, itertools
 from django.shortcuts import render
 
 # Create your views here.
 # Request handler - request -> response
 
-from django.http import HttpResponse
-from .models import Club
-from django.template import loader
-
 def index(request):
-    template = loader.get_template("index.html")
-    return HttpResponse(template.render())
+    return render(request, 'index.html')
 

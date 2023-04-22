a8b9acbc3ec7800044f19aa3624321162ce00466
a-b-e-q <27394198+a-b-e-q@users.noreply.github.com>
2023-04-22 15:22:49 -0500

    [Legacy commit squashed]

--- clubsite/list/views.py
+++ clubsite/list/views.py
@@ -9,13 +9,10 @@ from .models import Club
 from django.template import loader
 
 def index(request):
-    random_clubs = []
+    random_clubs = Club.get_random_club()
     template = loader.get_template("index.html")
     context = {
         "random_clubs": random_clubs,
     }
-    for _ in range(3):
-        random_club = Club.get_random_club()
-        random_clubs.append(random_club)
     return HttpResponse(template.render(context))
 

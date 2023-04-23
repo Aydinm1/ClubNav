433c91a76499321c8b66165be13138e07fa41e39
a-b-e-q <27394198+a-b-e-q@users.noreply.github.com>
2023-04-23 15:55:29 -0500

    [Legacy commit squashed]

--- clubsite/list/admin.py
+++ clubsite/list/admin.py
@@ -2,14 +2,19 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Club, Categories, SponsorName, MeetingRooms, ClubPresidentName
+from .models import Club, Categories, SponsorName, MeetingRooms, ClubPresidentName, OtherImage
+
+class OtherImageAdmin(admin.StackedInline):
+    model = OtherImage
 
 class ClubAdmin(admin.ModelAdmin):
     model = Club
     filter_horizontal = ('sponsor', 'category', 'meeting_room','club_president',)
+    inlines = [OtherImageAdmin]
 
 admin.site.register(Club, ClubAdmin)
 admin.site.register(Categories)
 admin.site.register(SponsorName)
 admin.site.register(MeetingRooms)
 admin.site.register(ClubPresidentName)
+admin.site.register(OtherImage)

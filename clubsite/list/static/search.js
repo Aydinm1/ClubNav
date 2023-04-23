dde82da7b931a5390bcd66b593fc1d6eaa56fe50
BuildTools <unconfigured@null.spigotmc.org>
2023-04-23 18:24:47 -0500

    [Legacy commit squashed]

--- clubsite/list/static/search.js
+++ clubsite/list/static/search.js
@@ -1,27 +1,24 @@
 console.log("test");
 
-// function myFunction() {
-//     var input, filter, cards, cardContainer, title, i, date;
-//     input = document.getElementById("myFilter");
-//     filter = input.value.toUpperCase();
-//     cardContainer = document.getElementById("bottomMid");
-//     cards = cardContainer.getElementsByClassName("cardOne");
+function myFunction() {
+    var input, filter, cards, cardContainer, title, i;
+    input = document.getElementById("myFilter");
+    filter = input.value.toUpperCase();
+    cardContainer = document.getElementById("bottomMid");
+    cards = cardContainer.getElementsByClassName("cardOne");
       
     
-//     for (i = 0; i < cards.length; i++) {
-//       title = cards[i].querySelector(".card-title");
-      
-//       if (title.innerText.toUpperCase().indexOf(filter) > -1) {
-//         cards[i].style.display = "";
-//       } 
-//       else if(date.innerText.toUpperCase().indexOf(filter) > -1){
-//         cards[i].style.display = "";
-//       }
+    for (i = 0; i < cards.length; i++) {
+      title = cards[i].querySelector(".card-title");
+
+      if(title, title.innerText.toUpperCase().indexOf(filter) > -1) {
+        cards[i].style.display = "";
+      } 
       
       
-//       else {
-//         cards[i].style.display = "none";
-//       }
-//     }
+      else {
+        cards[i].style.display = "none";
+      }
+    }
      
-//   }
+  }

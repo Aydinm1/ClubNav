6fbb4821ebe0da2124c5df0208c7277f5266d31d
a-b-e-q <27394198+a-b-e-q@users.noreply.github.com>
2023-04-23 23:31:38 -0500

    [Legacy commit squashed]

--- clubsite/list/views.py
+++ clubsite/list/views.py
@@ -2,7 +2,7 @@
 # Request handler - request -> response
 
 from django.http import HttpResponse
-from .models import Club, SponsorName, Categories
+from .models import Club, SponsorName, Categories, MeetingRooms
 from django.template import loader
 from django.shortcuts import get_object_or_404
 
@@ -19,9 +19,11 @@ def index(request):
 def clubtemplate(request, club_id):
     clubs = get_object_or_404(Club, pk=club_id)
     sponsors = SponsorName.objects.filter(club=club_id)
+    meeting_room = MeetingRooms.objects.filter(club=club_id)
     context = {
         "club": clubs,
-        "sponsors": sponsors
+        "sponsors": sponsors,
+        "meeting_room": meeting_room
     }
     template = loader.get_template("club-template.html")
     return HttpResponse(template.render(context))

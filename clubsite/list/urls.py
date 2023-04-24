a1dabf3da2f04cffe01ab22dc5179c9119aa97bf
a-b-e-q <27394198+a-b-e-q@users.noreply.github.com>
2023-04-23 19:13:55 -0500

    [Legacy commit squashed]

--- clubsite/list/urls.py
+++ clubsite/list/urls.py
@@ -7,5 +7,5 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('<int:club_id>/', views.clubtemplate, name = 'club-template'),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

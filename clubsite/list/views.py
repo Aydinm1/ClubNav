020ab584e9c22fd40820629700cd50fb5c0455d0
a-b-e-q <27394198+a-b-e-q@users.noreply.github.com>
2023-04-23 19:13:55 -0500

    [Legacy commit squashed]

--- clubsite/list/views.py
+++ clubsite/list/views.py
@@ -2,22 +2,26 @@
 # Request handler - request -> response
 
 from django.http import HttpResponse
-from .models import Club
+from .models import Club, SponsorName, Categories
 from django.template import loader
 from django.shortcuts import get_object_or_404
 
 def index(request):
     random_clubs = Club.get_random_club()
+    getCategories = Categories.objects.all()
     template = loader.get_template("index.html")
     context = {
         "random_clubs": random_clubs,
+        "getCategories": getCategories
     }
     return HttpResponse(template.render(context))
 
 def clubtemplate(request, club_id):
     clubs = get_object_or_404(Club, pk=club_id)
+    sponsor = SponsorName.objects.filter(club=club_id)
     context = {
-        "club": clubs
+        "club": clubs,
+        "sponsor": sponsor
     }
     template = loader.get_template("club-template.html")
     return HttpResponse(template.render(context))

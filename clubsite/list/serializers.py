8859ff9da7628d2f7144d63f0f8cb83557061e27
a-b-e-q <27394198+a-b-e-q@users.noreply.github.com>
2023-05-29 13:47:37 -0500

    [Legacy commit squashed]

--- clubsite/list/serializers.py
+++ clubsite/list/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Club
+from .models import Club, SponsorName
 
 class ClubSerializer(serializers.ModelSerializer):
     class Meta:
         model = Club
         fields = ('id', 'name', 'description', 'sponsor', 'club_president', 'category', 'meeting_room', 'start_time', 'end_time')
+
+class SponsorNameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SponsorName 
+        fields = ('sponsor_name', 'sponsor_email')

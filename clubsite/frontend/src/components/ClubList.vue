5c516f57b46f8199b67f2678847364bd6c667047
a-b-e-q <27394198+a-b-e-q@users.noreply.github.com>
2023-06-15 19:54:46 -0500

    [Legacy commit squashed]

--- clubsite/frontend/src/components/ClubList.vue
+++ clubsite/frontend/src/components/ClubList.vue
@@ -1,21 +1,59 @@
-<script setup>
-import axios from 'axios'
-</script>
-
 <template>
-  <div class="item">
-    <i>
-      <slot name="icon"></slot>
-    </i>
-    <div class="details">
-      <h3>
-        <slot name="heading"></slot>
-      </h3>
-      <slot></slot>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+  <div id="bottomMid"> 
+        
+    <div class="cardOne">
+        <div class="imageHolder">
+            <img class="card-img-top" src="{{ club.main_picture }}" alt="Card image cap">
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">{{ club.name }}</h5>
+            <p class="card-text">{{ club.description }}</p>
+            <a class="btn btn-primary">Learn More</a>
+        </div>
     </div>
+
   </div>
 </template>
 
-<style scoped>
+<script>
+import axios from 'axios'
 
+export default {
+  data() {
+    return {
+      posts: [],
+      errors: []
+    }
+  },
+
+  // Fetches posts when the component is created.
+  async created() {
+    try {
+      const response = await axios.get(`http://localhost:8000/club/`, {
+        headers: {
+          Accept: 'application/json'
+        }
+      })
+      this.club = response.data
+    } catch (e) {
+      this.errors.push(e)
+    }
+  }
+}
+</script>
+
+<style scoped>
+#topMid{
+    height:25%;
+    display:flex;
+    justify-content:center;
+    align-items: center;
+}
+
+#mid{
+    width:100%;
+    display:flex;
+}
 </style>
